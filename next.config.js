bfdcc0a85d54ee5f26214965caf6738003409f8c
Chinenye Oguadinma <107203528+ChinenyeOguadinma@users.noreply.github.com>
2025-10-17 10:51:19 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,37 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   images: {
-    unoptimized: true
-  }
-}
+    domains: ['non-linearcareer.com'], // Replace with your domain if images are external
+    formats: ['image/avif', 'image/webp'],
+    minimumCacheTTL: 60,
+    disableStaticImages: false,
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '3000',
+        pathname: '/book cover.jpg', // Adjust pathname as needed
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '3000',
+        pathname: '/IMG_0373 Medium 2.jpeg', // Adjust pathname as needed
+      },
+       {
+        protocol: 'http',
+        hostname: '127.0.0.1',
+        port: '3000',
+        pathname: '/book cover.jpg', // Adjust pathname as needed
+      },
+      {
+        protocol: 'http',
+        hostname: '127.0.0.1',
+        port: '3000',
+        pathname: '/IMG_0373 Medium 2.jpeg', // Adjust pathname as needed
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
