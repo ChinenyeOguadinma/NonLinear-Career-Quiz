e78c34c3ae20932ea14e6f41e748b01fedbc556e
Chinenye Oguadinma <107203528+ChinenyeOguadinma@users.noreply.github.com>
2025-10-24 08:23:29 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,8 @@ import './globals.css'
 import Navigation from './components/Navigation'
 import Footer from './components/Footer'
 import Script from 'next/script'
+import { SpeedInsights } from '@vercel/speed-insights/next'
+ 
 
 export const metadata = {
   title: 'The Non-Linear Career Workbook | Non-Linear Careers',
@@ -25,6 +27,11 @@ export default function RootLayout({ children }) {
             gtag('config', 'G-CZ7SDZLEQG');
           `}
         </Script>
+          <Script
+          src="https://va.vercel-scripts.com/v1/speed-insights/script.js"
+          strategy="afterInteractive"
+          data-endpoint="/_vercel/speed-insights"
+        />
       </head>
       <body>
         <Navigation />
